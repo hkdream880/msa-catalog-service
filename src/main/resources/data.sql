73837b9bf1a1133a663fb0864bb1c1f86b13e2c0
이현국 <hklee@sptek.co.kr>
2021-04-21 17:39:21 +0900

    'order-service-init'

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-001', 'Berlin', 100, 1500);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-002', 'Seoul', 110, 1300);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-003', 'New york', 105, 1200);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-004', 'Chicago', 120, 1400);
